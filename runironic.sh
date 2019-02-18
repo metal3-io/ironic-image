6cbb7de64850380f9a2b8d04be94403f9e4fffe1
Dmitry Tantsur <divius.inside@gmail.com>
2019-02-18 11:55:04 +0100

    Switch to JSON RPC instead of Rabbit MQ

--- runironic.sh
+++ runironic.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/bash
-/usr/lib/rabbitmq/bin/rabbitmq-server > /var/log/rabbit.out 2>&1 &
 /usr/bin/python2 /usr/bin/ironic-conductor > /var/log/ironic-conductor.out 2>&1 &
 /usr/bin/python2 /usr/bin/ironic-api > /var/log/ironic-api.out 2>&1 &
 /usr/sbin/httpd
