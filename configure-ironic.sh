eeb8065a7322339dced4b8899db8c13f4286f7ec
Stephen Benjamin <stephen@redhat.com>
2019-10-17 14:13:49 -0400

    Enable IPv6 Support
    This commit adds support for IPv6. When $PROVISIONING_IP is specified,
    which may be an IPv6 address, the various containers will wait for that
    IP to become available on an interface.
    
    If the IP is IPv6, then we use an IPv6-specific configurations. Note,
    IPv6 hosts are expected to be using UEFI boot, as we use snponly.efi.
    snponly.efi uses the UEFI network stack instead of the iPXE drivers.
    When using EDK2 OVMF + iPXE + ipxe.efi, we have seen lock-ups in
    initialization of the hardware devices.
    
    As neither CentOS nor RHEL distribute iPXE builds with IPv6 support, we
    build them from source as part of the container build.

--- configure-ironic.sh
+++ configure-ironic.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/bash
 
-# Get environment settings and update ironic.conf
-PROVISIONING_INTERFACE=${PROVISIONING_INTERFACE:-"provisioning"}
-IRONIC_IP=$(ip -4 address show dev "$PROVISIONING_INTERFACE" | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | head -n 1)
-until [ ! -z "${IRONIC_IP}" ]; do
-  echo "Waiting for ${PROVISIONING_INTERFACE} interface to be configured"
-  sleep 1
-  IRONIC_IP=$(ip -4 address show dev "$PROVISIONING_INTERFACE" | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | head -n 1)
-done
+. /bin/ironic-common.sh
+
 HTTP_PORT=${HTTP_PORT:-"80"}
 MARIADB_PASSWORD=${MARIADB_PASSWORD:-"change_me"}
 NUMPROC=$(cat /proc/cpuinfo  | grep "^processor" | wc -l)
@@ -16,6 +10,8 @@ NUMWORKERS=$(( NUMPROC < 12 ? NUMPROC : 12 ))
 # Whether to enable fast_track provisioning or not
 IRONIC_FAST_TRACK=${IRONIC_FAST_TRACK:-true}
 
+wait_for_interface_or_ip
+
 cp /etc/ironic/ironic.conf /etc/ironic/ironic.conf_orig
 
 crudini --merge /etc/ironic/ironic.conf <<EOF
@@ -23,20 +19,21 @@ crudini --merge /etc/ironic/ironic.conf <<EOF
 my_ip = $IRONIC_IP
 
 [api]
+host_ip = ::
 api_workers = $NUMWORKERS
 
 [conductor]
-api_url = http://${IRONIC_IP}:6385
+api_url = http://${IRONIC_URL_HOST}:6385
 
 [database]
 connection = mysql+pymysql://ironic:${MARIADB_PASSWORD}@localhost/ironic?charset=utf8
 
 [deploy]
-http_url = http://${IRONIC_IP}:${HTTP_PORT}
+http_url = http://${IRONIC_URL_HOST}:${HTTP_PORT}
 fast_track = ${IRONIC_FAST_TRACK}
 
 [inspector]
-endpoint_override = http://${IRONIC_IP}:5050
+endpoint_override = http://${IRONIC_URL_HOST}:5050
 
 [mdns]
 interfaces = $IRONIC_IP
--- runhealthcheck.sh
+++ runhealthcheck.sh
@@ -15,7 +15,7 @@ while true ; do
 
     elif [ $1 = "dnsmasq" ] ; then
        DNSMASQPID=$(pidof dnsmasq)
-       fuser 67/udp |& grep -w "$DNSMASQPID"
+       fuser 67/udp 547/udp |& grep -w "$DNSMASQPID"
 
     elif [ $1 = "ironic" ] ; then
        curl -s http://localhost:6385 > /dev/null || ( echo "Can't contact ironic-api" && exit 1 )
