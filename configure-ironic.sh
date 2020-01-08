2505ff68563544e70bcba1a5ea6c1d11a6ad71f2
maelk <mael.kimmerlin@est.tech>
2020-01-08 12:47:28 +0200

    add HTTP port in bootloader configuration for ironic
    It was missing the port, always going to port 80 when httpd
    listens on 6180 in BMO deployments

--- configure-ironic.sh
+++ configure-ironic.sh
@@ -33,7 +33,7 @@ host_ip = ::
 api_workers = $NUMWORKERS
 
 [conductor]
-bootloader = http://${IRONIC_URL_HOST}/uefi_esp.img
+bootloader = http://${IRONIC_URL_HOST}:${HTTP_PORT}/uefi_esp.img
 
 [database]
 connection = mysql+pymysql://ironic:${MARIADB_PASSWORD}@localhost/ironic?charset=utf8
