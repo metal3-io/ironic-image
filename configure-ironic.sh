f7f37285ae4473bd881407dbd6189d727a235430
Julia Kreger <juliaashleykreger@gmail.com>
2020-07-01 10:37:23 -0700

    Add a managed introspection IPA url
    Managed introspection is modeled around powering on the machine
    updating the inspection data, and powering it off.
    
    However when features like fast track are used, inspection does not
    know how to check-in with ironic. As such, we should supply it.

--- configure-ironic.sh
+++ configure-ironic.sh
@@ -38,6 +38,7 @@ fast_track = ${IRONIC_FAST_TRACK}
 
 [inspector]
 endpoint_override = http://${IRONIC_URL_HOST}:5050
+extra_kernel_params = ipa-api-url=http://${IRONIC_URL_HOST}:6385
 
 [service_catalog]
 endpoint_override = http://${IRONIC_URL_HOST}:6385
