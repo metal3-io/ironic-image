0ec3bfd1a3429743ccc66d90a47f1959c9059443
Riccardo Pittau <elfosardo@gmail.com>
2020-11-24 12:44:32 +0100

    Introduce separate ARG for extra packages list
    Convert the current PKGS_LIST to ENV as we should not really touch it.
    Introduce the EXTRA_PKGS_LIST ARG to allow specifying a file
    containing a list of extra packages to install.
    The goal is to allow installing packages from different sources
    to test them.
    
    (cherry picked from commit a411a5aa6e9571e4095d52cc0d7e73ada6c2a9c7)

--- prepare-image.sh
+++ prepare-image.sh
@@ -3,9 +3,14 @@
 set -ex
 
 dnf upgrade -y
-dnf --setopt=install_weak_deps=False install -y $(cat /tmp/main-packages-list.txt)
+dnf --setopt=install_weak_deps=False install -y $(cat /tmp/${PKGS_LIST})
 if [ $(uname -m) = "x86_64" ]; then
     dnf install -y syslinux-nonlinux;
+fi
+if [[ ! -z ${EXTRA_PKGS_LIST} ]]; then
+    if [[ -s /tmp/${EXTRA_PKGS_LIST} ]]; then
+        dnf --setopt=install_weak_deps=False install -y $(cat /tmp/${EXTRA_PKGS_LIST})
+    fi
 fi
 dnf clean all
 rm -rf /var/cache/{yum,dnf}/*
