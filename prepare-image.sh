35c66c8c3782e576a0d3df59497aeeff7b3f8941
Riccardo Pittau <elfosardo@gmail.com>
2020-12-14 17:35:29 +0100

    Explicitly set minimum versions of python libraries
    To simplify keeping track of minimum installed and required version
    of python libraries and ironic packages, we add the versions in
    the packages list.
    This will also help when cross-tagging packages to test with the
    new versions using the prevalidation repositories.

--- prepare-image.sh
+++ prepare-image.sh
@@ -3,13 +3,13 @@
 set -euxo pipefail
 
 dnf upgrade -y
-dnf --setopt=install_weak_deps=False install -y $(cat /tmp/${PKGS_LIST})
+xargs -rd'\n' dnf --setopt=install_weak_deps=False install -y < /tmp/${PKGS_LIST}
 if [ $(uname -m) = "x86_64" ]; then
     dnf install -y syslinux-nonlinux;
 fi
 if [[ ! -z ${EXTRA_PKGS_LIST:-} ]]; then
     if [[ -s /tmp/${EXTRA_PKGS_LIST} ]]; then
-        dnf --setopt=install_weak_deps=False install -y $(cat /tmp/${EXTRA_PKGS_LIST})
+        xargs -rd'\n' dnf --setopt=install_weak_deps=False install -y < /tmp/${EXTRA_PKGS_LIST}
     fi
 fi
 dnf clean all
