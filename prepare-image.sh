1c206964546bb18b0ddca8b47dd4bfe1a2a9ae1e
Riccardo Pittau <elfosardo@gmail.com>
2020-12-15 09:46:33 +0100

    Prevent unbound variable error in prepare-image
    Also make build process fail if something goes wrong in prepare-image

--- prepare-image.sh
+++ prepare-image.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/bash
 
-set -ex
+set -euxo pipefail
 
 dnf install -y python3 python3-requests
 curl https://raw.githubusercontent.com/openstack/tripleo-repos/master/tripleo_repos/main.py | python3 - -b master current-tripleo
 dnf upgrade -y
 dnf --setopt=install_weak_deps=False install -y $(cat /tmp/${PKGS_LIST})
-if [[ ! -z ${EXTRA_PKGS_LIST} ]]; then
+if [[ ! -z ${EXTRA_PKGS_LIST:-} ]]; then
     if [[ -s /tmp/${EXTRA_PKGS_LIST} ]]; then
         dnf --setopt=install_weak_deps=False install -y $(cat /tmp/${EXTRA_PKGS_LIST})
     fi
 fi
 dnf clean all
 rm -rf /var/cache/{yum,dnf}/*
-if [[ ! -z ${PATCH_LIST} ]]; then
+if [[ ! -z ${PATCH_LIST:-} ]]; then
     if [[ -s "/tmp/${PATCH_LIST}" ]]; then
         /bin/patch-image.sh;
     fi
 fi
 rm -f /bin/patch-image.sh
+
