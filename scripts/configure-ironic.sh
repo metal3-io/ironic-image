d0db1d5bf63377f6986d6a0a914bf2d0c64cb5a0
Nam Xuan Nguyen <nam.xuan.nguyen@est.tech>
2021-01-27 14:53:14 +0200

    Run ironic-api as WSGI when standalone with TLS capability

--- scripts/configure-ironic.sh
+++ scripts/configure-ironic.sh
@@ -27,7 +27,6 @@ fi
 
 . /bin/ironic-common.sh
 
-export HTTP_PORT=${HTTP_PORT:-"80"}
 export MARIADB_PASSWORD=${MARIADB_PASSWORD:-"change_me"}
 # TODO(dtantsur): remove the explicit default once we get
 # https://review.opendev.org/761185 in the repositories
@@ -35,7 +34,6 @@ NUMPROC=$(cat /proc/cpuinfo  | grep "^processor" | wc -l)
 NUMPROC=$(( NUMPROC <= 4 ? NUMPROC : 4 ))
 export NUMWORKERS=${NUMWORKERS:-$NUMPROC}
 export LISTEN_ALL_INTERFACES="${LISTEN_ALL_INTERFACES:-"true"}"
-export IRONIC_DEPLOYMENT="${IRONIC_DEPLOYMENT:-"Combined"}"
 
 # Whether to enable fast_track provisioning or not
 export IRONIC_FAST_TRACK=${IRONIC_FAST_TRACK:-true}
@@ -89,11 +87,6 @@ HTPASSWD_FILE=/etc/ironic/htpasswd
 # The user can provide HTTP_BASIC_HTPASSWD and HTTP_BASIC_HTPASSWD_RPC. If
 # - we are running conductor and HTTP_BASIC_HTPASSWD is set,
 #   use HTTP_BASIC_HTPASSWD for RPC.
-# - we are running combined and HTTP_BASIC_HTPASSWD is set, i.e. API is
-#   authenticated. We want to authenticate RPC by default, but the user might
-#   override. Then try to infere the authentication strategy and credentials
-#   from /auth/ironic-rpc/auth-config. If not present, then generate a username
-#   and password, create the config file the htpasswd content
 export JSON_RPC_AUTH_STRATEGY="noauth"
 if [ -n "${HTTP_BASIC_HTPASSWD}" ]; then
     if [ "${IRONIC_DEPLOYMENT}" == "Conductor" ]; then
@@ -104,52 +97,16 @@ if [ -n "${HTTP_BASIC_HTPASSWD}" ]; then
     fi
 fi
 
-
-# When running both API and Conductor in the same container, we'll try to get the credentials
-# from /auth/ironic-rpc/auth-config if present, or generate it
-if [ "${IRONIC_DEPLOYMENT}" == "Combined" ]; then
-    # We try to read the credentials from the config file as it is probably mounted read-only,
-    # We cannot modify it. If it is not set to basic, then do not authenticate the RPC. This is
-    # to ensure that the setup will work if the user gives a specific config for rpc set to no_auth
-    if [ -f "/auth/ironic-rpc/auth-config" ]; then
-        IRONIC_RPC_TMP_TYPE="$(crudini --get /auth/ironic-rpc/auth-config json_rpc auth_type)" || exit 1
-        if [ "${IRONIC_RPC_TMP_TYPE}" == "http_basic" ]; then
-            IRONIC_RPC_TMP_USERNAME="$(crudini --get /auth/ironic-rpc/auth-config json_rpc username)" || exit 1
-            IRONIC_RPC_TMP_PASSWORD="$(crudini --get /auth/ironic-rpc/auth-config json_rpc password)" || exit 1
-        else
-            export JSON_RPC_AUTH_STRATEGY="noauth"
-        fi
-    # We do not have an auth config file, so we generate one
-    else
-        IRONIC_RPC_TMP_USERNAME="rpc-user"
-        IRONIC_RPC_TMP_PASSWORD="$(tr -dc 'a-zA-Z0-9' < /dev/urandom | fold -w 12 | head -n 1)"
-        mkdir -p "/auth/ironic-rpc"
-        cat << EOF > "/auth/ironic-rpc/auth-config"
-[json_rpc]
-auth_type=http_basic
-username=${IRONIC_RPC_TMP_USERNAME}
-password=${IRONIC_RPC_TMP_PASSWORD}
-http_basic_username=${IRONIC_RPC_TMP_USERNAME}
-http_basic_password=${IRONIC_RPC_TMP_PASSWORD}
-EOF
-    fi
-
-    # Populate HTTP_BASIC_HTPASSWD_RPC
-    if [ -n "${IRONIC_RPC_TMP_USERNAME:-}" ]; then
-        htpasswd -n -b -B "${IRONIC_RPC_TMP_USERNAME}" "${IRONIC_RPC_TMP_PASSWORD}" >"${HTPASSWD_FILE}-rpc"
-    fi
-fi
-
 # The original ironic.conf is empty, and can be found in ironic.conf_orig
 render_j2_config /etc/ironic/ironic.conf.j2 /etc/ironic/ironic.conf
 
 # Configure auth for clients
-IRONIC_CONFIG_OPTIONS="--config-file /etc/ironic/ironic.conf"
-
 configure_client_basic_auth() {
     local auth_config_file="/auth/$1/auth-config"
     if [ -f ${auth_config_file} ]; then
-        IRONIC_CONFIG_OPTIONS+=" --config-file ${auth_config_file}"
+        # Merge configurations in the "auth" directory into the default ironic configuration file because there is no way to choose the configuration file 
+        # when running the api as a WSGI app.
+        crudini --merge "/etc/ironic/ironic.conf" < ${auth_config_file} 
     fi
 }
 
