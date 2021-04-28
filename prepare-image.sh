6560f4bf3b2b24b8f08086b452a12a4250f90882
Nam Xuan Nguyen <nam.xuan.nguyen@est.tech>
2021-04-28 17:35:26 +0300

    Fix no stream issue

--- prepare-image.sh
+++ prepare-image.sh
@@ -3,7 +3,7 @@
 set -euxo pipefail
 
 dnf install -y python3 python3-requests
-curl https://raw.githubusercontent.com/openstack/tripleo-repos/master/tripleo_repos/main.py | python3 - -b master current-tripleo
+curl https://raw.githubusercontent.com/openstack/tripleo-repos/master/tripleo_repos/main.py | python3 - -b master current-tripleo --no-stream
 dnf upgrade -y
 xargs -rtd'\n' dnf --setopt=install_weak_deps=False install -y < /tmp/${PKGS_LIST}
 if [[ ! -z ${EXTRA_PKGS_LIST:-} ]]; then
