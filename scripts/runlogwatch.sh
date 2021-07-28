bd700ea13683515f6dac422275cb359b8650fd01
Dmitry Tantsur <dtantsur@protonmail.com>
2021-07-28 09:46:58 +0200

    Rework runlogwatch.sh
    Currently it only waits for files in the first created directory because
    of an infinite loop inside the "for" loop.

--- scripts/runlogwatch.sh
+++ scripts/runlogwatch.sh
@@ -3,21 +3,21 @@
 # Ramdisk logs path
 LOG_DIRS=("/shared/log/ironic/deploy" "/shared/log/ironic-inspector/ramdisk")
 
-for LOG_DIR in ${LOG_DIRS[@]}; do
-    if [[ -d $LOG_DIR ]]; then
-        while :
-        do
-            until  ls "${LOG_DIR}"/*.tar.gz 1> /dev/null 2>&1
-            do
-                sleep 5
-            done
+while :
+do
+    for LOG_DIR in ${LOG_DIRS[@]}; do
+        if ! ls "${LOG_DIR}"/*.tar.gz 1> /dev/null 2>&1;
+        then
+            continue
+        fi
 
-            for fn in "${LOG_DIR}"/*.tar.gz
-            do
-                echo "************ Contents of $fn ramdisk log file bundle **************"
-                tar -xOzvvf $fn | sed -e "s/^/$(basename $fn): /"
-                rm -f $fn
-            done
+        for fn in "${LOG_DIR}"/*.tar.gz
+        do
+            echo "************ Contents of $fn ramdisk log file bundle **************"
+            tar -xOzvvf $fn | sed -e "s/^/$(basename $fn): /"
+            rm -f $fn
         done
-    fi
+    done
+
+    sleep 5
 done
