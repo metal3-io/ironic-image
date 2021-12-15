dc377082fc2b9b522b54247a4b801b1516a41d67
Zane Bitter <zbitter@redhat.com>
2021-12-15 12:23:26 -0500

    Use IPv6-friendly URLs for CoreOS

--- scripts/coreos-ipa-common.sh
+++ scripts/coreos-ipa-common.sh
@@ -5,9 +5,9 @@ IGNITION_FILE=${IGNITION_FILE:-/shared/html/ironic-python-agent.ign}
 ISO_FILE=${ISO_FILE:-/shared/html/images/ironic-python-agent.iso}
 
 function coreos_kernel_params {
-    echo -n "coreos.live.rootfs_url=http://$IRONIC_IP:$HTTP_PORT/images/ironic-python-agent.rootfs"
+    echo -n "coreos.live.rootfs_url=http://${IRONIC_URL_HOST}:$HTTP_PORT/images/ironic-python-agent.rootfs"
     if [ -f "$IGNITION_FILE" ]; then
-        echo -n " ignition.config.url=http://$IRONIC_IP:$HTTP_PORT/ironic-python-agent.ign"
+        echo -n " ignition.config.url=http://${IRONIC_URL_HOST}:$HTTP_PORT/ironic-python-agent.ign"
     fi
     echo " ignition.firstboot ignition.platform.id=metal"
 }
