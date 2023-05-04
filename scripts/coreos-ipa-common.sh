02a56a99570b59005b5319f1f5fd83dfe6934c8e
Tuomo Tanskanen <tuomo.tanskanen@est.tech>
2023-05-04 15:50:00 +0300

    add shellcheck and fix complaints
    Add shellcheck. Fix all the shellcheck complaints.
    
    Another PR in project-infra will activate the check for PRs.

--- scripts/coreos-ipa-common.sh
+++ scripts/coreos-ipa-common.sh
@@ -4,18 +4,24 @@ ROOTFS_FILE=${ROOTFS_FILE:-/shared/html/images/ironic-python-agent.rootfs}
 IGNITION_FILE=${IGNITION_FILE:-/shared/html/ironic-python-agent.ign}
 ISO_FILE=${ISO_FILE:-/shared/html/images/ironic-python-agent.iso}
 
-function coreos_kernel_params {
+coreos_kernel_params()
+{
     echo -n "coreos.live.rootfs_url=http://${IRONIC_URL_HOST}:$HTTP_PORT/images/ironic-python-agent.rootfs"
-    if [ -f "$IGNITION_FILE" ]; then
+    if [[ -f "$IGNITION_FILE" ]]; then
         echo -n " ignition.config.url=http://${IRONIC_URL_HOST}:$HTTP_PORT/ironic-python-agent.ign"
     fi
     echo " ignition.firstboot ignition.platform.id=metal"
 }
 
-function use_coreos_ipa {
-    [ -f "$ROOTFS_FILE" ] && return 0 || return 1
+use_coreos_ipa()
+{
+    if [[ -f "$ROOTFS_FILE" ]]; then
+        return 0
+    fi
+    return 1
 }
 
 if use_coreos_ipa; then
-    export IRONIC_KERNEL_PARAMS="${IRONIC_KERNEL_PARAMS:-} $(coreos_kernel_params)"
+    IRONIC_KERNEL_PARAMS="${IRONIC_KERNEL_PARAMS:-} $(coreos_kernel_params)"
+    export IRONIC_KERNEL_PARAMS
 fi
--- scripts/runlogwatch.sh
+++ scripts/runlogwatch.sh
@@ -3,19 +3,16 @@
 # Ramdisk logs path
 LOG_DIRS=("/shared/log/ironic/deploy" "/shared/log/ironic-inspector/ramdisk")
 
-while :
-do
-    for LOG_DIR in ${LOG_DIRS[@]}; do
-        if ! ls "${LOG_DIR}"/*.tar.gz 1> /dev/null 2>&1;
-        then
+while :; do
+    for LOG_DIR in "${LOG_DIRS[@]}"; do
+        if ! ls "${LOG_DIR}"/*.tar.gz 1> /dev/null 2>&1; then
             continue
         fi
 
-        for fn in "${LOG_DIR}"/*.tar.gz
-        do
+        for fn in "${LOG_DIR}"/*.tar.gz; do
             echo "************ Contents of $fn ramdisk log file bundle **************"
-            tar -xOzvvf $fn | sed -e "s/^/$(basename $fn): /"
-            rm -f $fn
+            tar -xOzvvf "$fn" | sed -e "s/^/$(basename "$fn"): /"
+            rm -f "$fn"
         done
     done
 
