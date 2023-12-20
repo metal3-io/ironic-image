66cad194184b49382751c021444df93fd41f52f4
Riccardo Pittau <elfosardo@gmail.com>
2023-12-20 10:44:56 +0100

    Install ironic and ironic-inspector from source
    Stop using RPMs for ironic and ironic-inspector and install all
    their services from source code directly, using the openshift
    forks.

--- prepare-image.sh
+++ prepare-image.sh
@@ -18,6 +18,49 @@ if [[ -n "${EXTRA_PKGS_LIST:-}" ]]; then
     fi
 fi
 
+### cachito magic works for OCP only
+if  [[ -f /tmp/main-packages-list.ocp ]]; then
+
+    REQS="${REMOTE_SOURCES_DIR}/requirements.cachito"
+
+    ls -la "${REMOTE_SOURCES_DIR}/" # DEBUG
+
+    # load cachito variables only if they're available
+    if [[ -d "${REMOTE_SOURCES_DIR}/cachito-gomod-with-deps" ]]; then
+        source "${REMOTE_SOURCES_DIR}/cachito-gomod-with-deps/cachito.env"
+        REQS="${REMOTE_SOURCES_DIR}/cachito-gomod-with-deps/app/requirements.cachito"
+    fi
+
+    ### source install ###
+    BUILD_DEPS="python3-devel gcc gcc-c++"
+
+    dnf install -y python3-pip python3-setuptools $BUILD_DEPS
+
+    # NOTE(elfosardo): --no-index is used to install the packages emulating
+    # an isolated environment in CI. Do not use the option for downstream
+    # builds.
+    PIP_OPTIONS=""
+    if [[ ! -d "${REMOTE_SOURCES_DIR}/cachito-gomod-with-deps" ]]; then
+        PIP_OPTIONS="--no-index"
+    fi
+    python3 -m pip install $PIP_OPTIONS --prefix /usr -r "${REQS}"
+
+    # ironic and ironic-inspector system configuration
+    mkdir -p /var/log/ironic /var/log/ironic-inspector /var/lib/ironic /var/lib/ironic-inspector
+    getent group ironic >/dev/null || groupadd -r ironic
+    getent passwd ironic >/dev/null || useradd -r -g ironic -s /sbin/nologin ironic -d /var/lib/ironic
+    getent group ironic-inspector >/dev/null || groupadd -r ironic-inspector
+    getent passwd ironic-inspector >/dev/null || useradd -r -g ironic-inspector -s /sbin/nologin ironic-inspector -d /var/lib/ironic-inspector
+
+    dnf remove -y $BUILD_DEPS
+
+    if [[ -d "${REMOTE_SOURCES_DIR}/cachito-gomod-with-deps" ]]; then
+        rm -rf $REMOTE_SOURCES_DIR
+    fi
+
+fi
+###
+
 chown ironic:ironic /var/log/ironic
 # This file is generated after installing mod_ssl and it affects our configuration
 rm -f /etc/httpd/conf.d/ssl.conf /etc/httpd/conf.d/autoindex.conf /etc/httpd/conf.d/welcome.conf /etc/httpd/conf.modules.d/*.conf
