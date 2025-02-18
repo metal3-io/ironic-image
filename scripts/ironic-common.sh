26c71bbffc73b4ebc72b753d600aea53a30147b6
Brian Oldfield <boldfield@nvidia.com>
2025-02-18 11:11:32 -0800

    Allow override of `http_url`, etc for different network topologies.  Fixes #627
    Signed-off-by: Brian Oldfield <boldfield@nvidia.com>

--- scripts/ironic-common.sh
+++ scripts/ironic-common.sh
@@ -2,7 +2,9 @@
 
 set -euxo pipefail
 
-IRONIC_IP="${IRONIC_IP:-}"
+# Export IRONIC_IP to avoid needing to lean on IRONIC_URL_HOST for consumption in
+# e.g. dnsmasq configuration
+export IRONIC_IP="${IRONIC_IP:-}"
 PROVISIONING_INTERFACE="${PROVISIONING_INTERFACE:-}"
 PROVISIONING_IP="${PROVISIONING_IP:-}"
 PROVISIONING_MACS="${PROVISIONING_MACS:-}"
@@ -69,6 +71,12 @@ wait_for_interface_or_ip()
         export IPV=4
         export IRONIC_URL_HOST="$IRONIC_IP"
     fi
+
+    # Avoid having to construct full URL multiple times while allowing
+    # the override of IRONIC_HTTP_URL for environments in which IRONIC_IP
+    # is unreachable from hosts being provisioned.
+    export IRONIC_HTTP_URL="${IRONIC_HTTP_URL:-http://${IRONIC_URL_HOST}:${HTTP_PORT}}"
+    export IRONIC_TFTP_URL="${IRONIC_HTTP_URL:-tftp://${IRONIC_URL_HOST}"
 }
 
 render_j2_config()
