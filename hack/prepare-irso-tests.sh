f859c1b18b3d0993b11d5b688c2044f994c2670b
Dmitry Tantsur <dtantsur@protonmail.com>
2025-08-21 15:35:38 +0200

    Synchronize the functional job with IrSO
    Use the IrSO's run.sh instead of the local version. This fixes a few
    issues, such as missing verbosity or lack of filtering.
    
    Rename the setup script to something more suitable and split it into
    a separate workflow step.
    
    Signed-off-by: Dmitry Tantsur <dtantsur@protonmail.com>

--- hack/prepare-irso-tests.sh
+++ hack/prepare-irso-tests.sh
@@ -7,14 +7,7 @@ cd "${REPO_ROOT}" || exit 1
 
 CLUSTER_TYPE="${CLUSTER_TYPE:-kind}"
 CONTAINER_RUNTIME="${CONTAINER_RUNTIME:-podman}"
-
-IRSO_REPO="${IRSO_REPO:-https://github.com/metal3-io/ironic-standalone-operator}"
-IRSO_BRANCH="${IRSO_BRANCH:-main}"
-if [[ -z "${IRSO_PATH:-}" ]]; then
-    IRSO_PATH="$(mktemp -td irso-XXXXXXXX)"
-    git clone "${IRSO_REPO}" -b "${IRSO_BRANCH}" "${IRSO_PATH}"
-fi
-export IRONIC_CUSTOM_IMAGE=localhost/ironic:test
+IRONIC_CUSTOM_IMAGE=${IRONIC_CUSTOM_IMAGE:-localhost/ironic:test}
 
 "${CONTAINER_RUNTIME}" build -t "${IRONIC_CUSTOM_IMAGE}" .
 IMAGE_ARCHIVE="$(mktemp --suffix=.tar)"
@@ -25,10 +18,3 @@ else
     minikube image load --logtostderr "${IMAGE_ARCHIVE}"
 fi
 rm -f "${IMAGE_ARCHIVE}"
-
-cd "${IRSO_PATH}/test"
-# shellcheck disable=SC1091
-. testing.env
-export IRONIC_CUSTOM_VERSION=latest
-
-exec go test -timeout 90m
