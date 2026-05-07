c1bf8f233a9f9483cd33a464778eae9a540491de
Nicolas Belouin <nicolas.belouin@suse.com>
2026-05-07 15:20:24 +0200

    Use placeholder in iPXE template for variable injection
    Replace sed append operation with a placeholder-based substitution
    for injecting Jinja2 variables into the iPXE template. This makes
    the template modification more explicit and maintainable.
    
    Fixes #971
    Implements change requested in #851 (comment)
    
    Signed-off-by: Nicolas Belouin <nicolas.belouin@suse.com>

--- scripts/configure-ironic.sh
+++ scripts/configure-ironic.sh
@@ -95,9 +95,7 @@ if [[ -f /proc/sys/crypto/fips_enabled ]]; then
 fi
 
 # inject TLS and cacert bundle variables in ipxe_template
-sed "1 a\
-\{%- set ipxe_tls_setup = $IRONIC_TLS_SETUP -%}\\
-\{%- set inject_cacert_bundle = $IRONIC_INJECT_IPA -%}" /templates/ipxe_config.template > "${IRONIC_CONF_DIR}/ipxe_config.template"
+sed "s/{# INJECT_VARIABLES #}/{% set ipxe_tls_setup = $IRONIC_TLS_SETUP %}\n{% set inject_cacert_bundle = $IRONIC_INJECT_IPA %}/" /templates/ipxe_config.template > "${IRONIC_CONF_DIR}/ipxe_config.template"
 
 # The original ironic.conf is empty, and can be found in ironic.conf.orig
 render_j2_config "/etc/ironic/ironic.conf.j2" \
